497ad10eab0cdb24f97507e7ad220eaf13b5a0af
ruichee <maoruijie@yahoo.com>
2026-01-10 22:44:34 +0800

    Added dealiasing

--- 1D_KS.py
+++ 1D_KS.py
@@ -3,8 +3,11 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 
-def dealiase(f):
-    pass
+def dealiase(ff, kx):
+    k = np.absolute(kx)
+    k_max = 1/3 * np.max(k)
+    ff_filtered = np.where(k <= k_max, ff, 0)
+    return ff_filtered
 
 
 def get_R(u, f, kx):
@@ -12,16 +15,22 @@ def get_R(u, f, kx):
     # non-linear term u∂ₓu in fourier space
     u_sq = u**2
     u_sqf = np.fft.fft(u_sq**2)
-    u_sqf_x = np.complex128(0, kx) * u_sqf
-    u_sq_x = np.fft.irfft(u_sqf_x)
+    u_sqf_x = -1j * kx * u_sqf
+    u_sq_x = np.fft.ifft(u_sqf_x)
     udu = -0.5 * u_sq_x
 
     # add linear terms in fourier space
     udu_f = np.fft.fft(udu)
     u_f = np.fft.fft(u)
-    u_f = dealiase(u_f)
-    R = udu_f - (-kx**2 + kx**4)*u_f
+    u_f = dealiase(u_f, kx)
+    R_f = udu_f - (-kx**2 + kx**4)*u_f
+    R_f = dealiase(R_f, kx)
     
+    # set mean flow = 0, i.e. wave has no DC component (constant offset)
+    R_f = np.where(kx == 0, 0, R_f)
+
+    # convert back to physical space
+    R = np.real(np.fft.ifft(R_f))
     return R
 
 
@@ -83,3 +92,13 @@ def main(u0, L, n, f, dt, n_iter_adj, n_iter_ngh, tol_adj, tol_ngh):
     plot_data()
 
 
+L = 22
+n = 128
+x = np.linspace(0, L, n)
+m=2
+u0=2*np.sin(m*2*np.pi*x/L)
+kx = 2*np.pi * np.fft.fftfreq(n, d=L/n)
+
+u = get_R(u0, 0, kx)
+plt.plot(u)
+plt.show()
