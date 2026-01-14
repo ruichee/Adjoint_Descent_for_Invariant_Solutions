e73d3dfdbed506041baab47fa6d5b08415a6fcf2
ruichee <maoruijie@yahoo.com>
2026-01-14 23:06:34 +0800

    Added plotting and compute_residuals()

--- 1D_KS.py
+++ 1D_KS.py
@@ -17,7 +17,9 @@ def get_vars(domain_size, num_colloc_pts):
 
 ###############################################################################################
 
-def dealiase(ff, kx):
+def dealiase(ff):
+    
+    global kx
 
     k = np.absolute(kx)
     k_max = 1/3 * np.max(k)                     # maximum frequency that we will keep
@@ -26,7 +28,9 @@ def dealiase(ff, kx):
 
 ###############################################################################################
 
-def get_R(u, f, kx): # TRY IMPLEMENTING VIA FINITE DIFFERENCE, VALIDATE IF FEASIBLE
+def get_R(u): # TRY IMPLEMENTING VIA FINITE DIFFERENCE, VALIDATE IF FEASIBLE
+
+    global kx, f
 
     # non-linear term -u∂ₓu in fourier space
     u_sq = u**2                                 # obtain u^2, since -u∂ₓu = -0.5*∂ₓ(u^2)
@@ -42,7 +46,7 @@ def get_R(u, f, kx): # TRY IMPLEMENTING VIA FINITE DIFFERENCE, VALIDATE IF FEASI
 
     # obtain u in fourier space
     u_f = np.fft.fft(u)                         # bring u into fourier
-    u_f = dealiase(u_f, kx)                     # dealise u
+    u_f = dealiase(u_f)                         # dealise u
 
     '''# non-linear term -1/2(∂ₓu)^2 in fourier space 
     u_x_f = 1j * kx * u_f                       # ∂ₓu in fourier, differentiate via multiply ik
@@ -50,10 +54,10 @@ def get_R(u, f, kx): # TRY IMPLEMENTING VIA FINITE DIFFERENCE, VALIDATE IF FEASI
     u_x_sq = -0.5 * u_x * u_x                   # get -1/2(∂ₓu)^2'''
 
     # add linear terms -∂ₓₓu-∂ₓₓₓₓu in fourier space 
-    udu_f = np.fft.fft(udu)               # bring u∂ₓu back to fourier
-    R_f = udu_f + (kx**2 - kx**4)*u_f        # add linear terms, n-derivative = multiply u by (ik)^n
-    R_f = dealiase(R_f, kx)                     # dealise R
-    
+    udu_f = np.fft.fft(udu)                     # bring u∂ₓu back to fourier
+    R_f = udu_f + (kx**2 - kx**4)*u_f           # add linear terms, n-derivative = multiply u by (ik)^n
+    R_f = dealiase(R_f)                         # dealise R
+
     # set mean flow = 0, no DC component/offset
     R_f = np.where(kx == 0, 0, R_f)             # ensures the sine wave has no constant component (k=0)
 
@@ -64,10 +68,12 @@ def get_R(u, f, kx): # TRY IMPLEMENTING VIA FINITE DIFFERENCE, VALIDATE IF FEASI
 
 ###############################################################################################
 
-def get_G(u, f, kx):
+def get_G(u):
+
+    global kx, f
 
     # first obtain R and its fourier transform
-    R = get_R(u, f, kx)
+    R = get_R(u)
     R_f = np.fft.fft(R)
 
     '''# non-linear term -∂ₓ(R∂ₓu) in fourier space
@@ -86,14 +92,37 @@ def get_G(u, f, kx):
 
     # add linear terms -∂ₓₓR-∂ₓₓₓₓR in fourier space
     G_f = nlt_f - (kx**2 - kx**4)*R_f
-    G_f = dealiase(G_f, kx)
+    G_f = dealiase(G_f)
     G = np.real(np.fft.ifft(G_f))
 
     return G
 
 ###############################################################################################
 
-def adj_descent(u0, f, T, dt, rtol, atol):
+def steady_state_event(t, u):
+
+    # rhs can either be get_R() or get_G()
+    dudt = get_G(u)
+
+    # compute R or G as a magnitude 
+    change_in_u = np.linalg.norm(dudt)
+
+    # set tolerance for ending iteration
+    global u_tol
+    tolerance = u_tol
+
+    # compare, if G or R < tol, end iteration
+    return change_in_u - tolerance
+
+# Configure the Event
+steady_state_event.terminal = True  # Stop the simulation when this event occurs
+steady_state_event.direction = -1   # Only trigger when going from positive -> negative
+
+###############################################################################################
+
+def adj_descent(u0, rtol, atol):
+
+    global f, T, dt
 
     # Set up the time interval
     nt = int(T / dt) + 1  
@@ -101,61 +130,68 @@ def adj_descent(u0, f, T, dt, rtol, atol):
 
     # Integration: use solve_ivp with method='BDF' to mimic ode15s (stiff solver)
     solution = solve_ivp(
-        fun=lambda t,u: get_G(u, f, kx),        # function that returns du/dt
+        fun=lambda t,u: get_G(u),               # function that returns du/dt
         t_span=(0, T),                          # (start_time, end_time)
-        y0=u0,                                   # Initial condition
+        y0=u0,                                  # Initial condition
         method='BDF',                           # 'BDF' or 'Radau' replaces ode15s
+        events=steady_state_event,              # determine when to end iteration early
         t_eval=tspan,                           # The specific time points returned
         rtol=rtol,                              # Relative tolerance
         atol=atol                               # Absolute tolerance
     )
 
-    # Update the variable
+    # Extract the output list of iteration values
     u_lst = solution.y.T 
+    t_lst = solution.t.T
 
-    return u_lst
-
-###############################################################################################
-
-def ngh_descent(u0, f, T, dt, n_iter, tol):
-
-    u_lst = [u0]
-    u = u0
-    
-    for _ in tqdm(range(n_iter)):
-
-        un = u.copy()
-        R = get_R(u, f, kx)
-        
-        u = un + dt*R # can implement rk45 later on
-        u_lst.append(u)
-
-        '''if R < tol:
-            break'''
-
-    return u, u_lst
+    # check for convergence
+    return u_lst, t_lst
 
 ###############################################################################################
 
-def plot_data(u_lst):
+def compute_residuals(u_lst):
+
+    G_lst = np.zeros(len(u_lst))
+
+    for i in range(len(u_lst)):
+        G_lst[i] = np.linalg.norm(get_G(u_lst[i]))
+
+    return G_lst
+
+###############################################################################################
+
+def plot_data(u_lst) -> None:
     
     def update():
         pass
     
-
     # animate convergence
     #ani = FuncAnimation(fig=fig, frames=update)
 
-    [plt.plot(u_lst[i]) for i in range(1, len(u_lst))]
-    plt.plot(u_lst[0], linestyle='--', color='red')
+    fig, (u_val, res) = plt.subplots(1, 2, figsize=(10, 5))
 
+    [u_val.plot(u_lst[i]) for i in range(1, len(u_lst))]
+    u_val.plot(u_lst[0], linestyle='--', color='red')
+    u_val.set_xlabel('x')
+    u_val.set_ylabel('u')
+    u_val.set_title('Steady Solution of 1D KS Equation')
+    
+    G_lst = compute_residuals(u_lst)
+    res.plot(G_lst)
+    res.semilogy()
+    res.set_xlabel('τ')
+    res.set_title('Residual of Adjoint Norm ||G(u)||')
+
+    fig.tight_layout()
     plt.show()
 
 ###############################################################################################
 
-def main(u0, L, n, f, T, dt, adj_rtol, adj_atol):
+def main(u0, adj_rtol, adj_atol) -> None:
 
-    u_lst = adj_descent(u0, f, T, dt, adj_rtol, adj_atol)
+    u_lst, t_lst = adj_descent(u0, adj_rtol, adj_atol)
+
+    print(t_lst[-1])
 
     
     # check if ngh descent is required here
@@ -169,19 +205,20 @@ def main(u0, L, n, f, T, dt, adj_rtol, adj_atol):
 ###############################################################################################
 
 # define variables 
-L = 22      # domain size
-n = 128     # number of collocation points
+L = 22          # domain size
+n = 128         # number of collocation points
+T = 1000        # max iteration time
+dt = 1          # iteration step 
+u_tol = 1e-6    # tolerance for converged u
 
 # obtain domain field (x), and fourier wave numbers kx
 x, kx = get_vars(domain_size=L, num_colloc_pts=n)
 
 # define initial conditions of field variable u
-m = 1
-u0 = 2*np.sin(m*2*np.pi*x/L)
+m = 1 
+u0 = 2*np.sin(m*2*np.pi*x/L)    # initial wave
+f = 0                           # forcing term
 
-
-#U = -(np.fft.ifft(np.fft.fft(u) * 1j * kx))
-
-main(u0, L, n, 0, T=200, dt=1, adj_rtol=1e-10, adj_atol=1e-10)
-
+# call to main function to execute descent
+main(u0, adj_rtol=1e-10, adj_atol=1e-10)
 
